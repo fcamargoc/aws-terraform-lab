b80cb0f59c92f631f7079aead7da536c7c42c013
franklin camargo <caicedofrank7@outlook.com>
2025-01-17 10:08:27 -0500

    se comprobo el archivo terraform

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = "ami-0df8c184d5f6ae949" # Amazon Linux 2 AMI
   instance_type = "t2.small"
   tags = {
     Name = "Terraform-Example"
