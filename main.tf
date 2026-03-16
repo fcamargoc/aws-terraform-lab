91935b379a08b6330ed44e780b58eb671615ef55
franklin camargo <caicedofrank7@outlook.com>
2026-03-16 16:52:44 -0500

    test: vulnerable IaC for Cortex scan v6

--- main.tf
+++ main.tf
@@ -1,15 +1,40 @@
-
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_security_group" "insecure_sg" {
+  name        = "insecure-sg"
+  description = "Security group inseguro"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0df8c184d5f6ae949" # Amazon Linux 2 AMI
-  instance_type = "t2.micro"
+  ami                    = "ami-0df8c184d5f6ae949"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.insecure_sg.id]
+
+  root_block_device {
+    encrypted = false
+  }
+
   tags = {
     Name = "Terraform-Example"
   }
-   metadata_options {
-     http_tokens = "required"
-   }
+}
 
+resource "aws_s3_bucket" "insecure_bucket" {
+  bucket = "mi-bucket-inseguro-cortex-test"
 }
